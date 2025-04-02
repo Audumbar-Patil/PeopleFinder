fdc6f15f34ad18a88e38fbaa5240131e886b479e
Audumbar-Patil <patil.audumbar.santosh@gmail.com>
2025-04-03 04:33:46 +0530

    next change

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/PeopleFinder/", 
-  build: {
-    outDir: "dist",
-  }
-});
+  plugins: [react()],
+})
